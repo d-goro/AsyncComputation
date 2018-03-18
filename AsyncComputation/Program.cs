31e0457064e03cd70c81741c5246b931ef317d7f
Dima <dmitryg@hp.com>
2018-03-18 11:25:15 +0200

    final commit

---
+++ AsyncComputation/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsyncComputation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                // Start computation.
+                Example();
+
+                Console.WriteLine("Start typing:");
+                // Handle user input.
+                string result = Console.ReadLine();
+                Console.WriteLine("You typed: " + result);
+            }
+        }
+
+
+        static async void Example()
+        {
+            var t = await Task.Run(() => Allocate());
+            Console.WriteLine("Compute: " + t);
+        }
+
+        static int Allocate()
+        {
+            // Compute total count of digits in strings.
+            int size = 0;
+            for (int z = 0; z < 100; z++)
+            {
+                for (int i = 0; i < 100000; i++)
+                {
+                    var value = i.ToString();
+                    size += value.Length;
+                }
+            }
+            return size;
+        }
+    }
+}
---
+++ HandleFileAsync/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HandleFileAsync
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var task = HandleFileAsync();
+
+            // Control returns here before HandleFileAsync returns. Prompt the user.
+            Console.WriteLine("Please wait patiently while I do something important.");
+
+            // Do something at the same time as the file is being read.
+            var line = Console.ReadLine();
+            Console.WriteLine("You entered (asynchronous logic): " + line);
+
+            // Wait for the HandleFile task to complete. Display its results.
+            task.Wait();
+            var x = task.Result;
+            Console.WriteLine("Count: " + x);
+
+            Console.WriteLine("[DONE]");
+            Console.ReadLine();
+        }
+
+        static async Task<int> HandleFileAsync()
+        {
+            string file = @"file.log";
+            Console.WriteLine("HandleFile enter");
+            int count = 0;
+
+            // Read in the specified file.
+            // ... Use async StreamReader method.
+            using (StreamReader reader = new StreamReader(file))
+            {
+                string v = await reader.ReadToEndAsync();
+
+                // ... Process the filed at a somehow.
+                count += v.Length;
+
+                // ... A slow-running computation.
+                //     Dummy code.
+                for (int i = 0; i < 10000; i++)
+                {
+                    int x = v.GetHashCode();
+                    if (x == 0)
+                    {
+                        count--;
+                    }
+                }
+            }
+            Console.WriteLine("HandleFile exit");
+            return count;
+        }
+    }
+}
